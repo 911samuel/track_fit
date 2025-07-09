babca8e7e1921cc2e886fec3c2ca6001c12c7543
Abdulkhaliq59 <kananuraabdulkhaliq59@gmail.com>
2025-07-09 19:08:30 +0200

    feat(OnBoardScreen): Implemented onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
-// import 'package:flutter/material.dart';
-// import "package:riverpod/riverpod.dart";
-// import 'core/theme/app_theme.dart';
-// import 'ui/onboarding/view/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+import 'core/theme/app_theme.dart';
+import 'ui/onboarding/view/onboarding_screen.dart';
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
-// void main() {
-//   runApp(const ProviderScope(child: FitTrackApp()));
-// }
+void main() {
+  runApp(const ProviderScope(child: FitTrackApp()));
+}
 
-// class FitTrackApp extends StatelessWidget {
-//   const FitTrackApp({super.key});
+class FitTrackApp extends StatelessWidget {
+  const FitTrackApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Fitness Tracker',
-//       theme: AppTheme.darkTheme,
-//       home: const OnboardingScreen(),
-//       debugShowCheckedModeBanner: false,
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fitness Tracker',
+      theme: AppTheme.darkTheme,
+      home: const OnboardingScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
